ed3d10c25913998a5db6edc7722eb9b4c5268ad6
gaofang <837682676@qq.com>
2017-07-12 10:22:25 +0800

    init

--- main.js
+++ main.js
@@ -3,11 +3,9 @@
  */
 const Koa = require('koa');
 const app = new Koa();
-
 // response
 app.use(ctx => {
   ctx.body = 'Hello Koa aadad';
 });
-
 app.listen(3000);
 console.log('app started at localhost:3000...');
